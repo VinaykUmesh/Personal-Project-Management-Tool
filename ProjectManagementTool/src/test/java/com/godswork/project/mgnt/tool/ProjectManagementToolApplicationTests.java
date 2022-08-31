7f0cf3498bd60f91bafcf4b828655e0ac0e003e8
umesh@8681 <umamaheswaran.n@securekloud.com>
2022-08-31 20:52:20 +0530

    Added CRUD

---
+++ ProjectManagementTool/src/test/java/com/godswork/project/mgnt/tool/ProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godswork.project.mgnt.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
